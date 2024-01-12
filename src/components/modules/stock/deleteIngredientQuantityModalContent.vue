2a64eb89d58111ebd1cc74d51a3f367d3f3c9562
Saymon Felipe <linnubr@gmail.com>
2024-01-12 20:36:26 -0300

    Melhoria na plotagem de unidade de medida do estoque

--- src/components/modules/stock/deleteIngredientQuantityModalContent.vue
+++ src/components/modules/stock/deleteIngredientQuantityModalContent.vue
@@ -3,14 +3,14 @@
         <form class="edit-roles" id="informations-form" @submit.prevent="saveStock()">
             <div class="form-group">
                 <label for="ingredient">Item</label>
-                <select id="ingredient" name="ingredient">
+                <select id="ingredient" name="ingredient" @change="selectThisIngredient()">
                     <option value="">Qualquer</option>
                     <option v-for="(item, index) in ingredients" :key="index" :value="item.id[1]">{{ item.nome[1] }}</option>
                 </select>
             </div>
             <div class="radio-group">
-                <label for="quantity">Quantidade</label>
-                <input type="text" name="quantity" id="quantity" required v-model="quantity" @keypress="quantity = formatDecimalValues(quantity)">
+                <label for="quantity">Quantidade {{ selected_ingredient_measure_unit != "" ? `(${selected_ingredient_measure_unit})` : "" }}</label>
+                <input type="text" name="quantity" id="quantity" required v-model="quantity" @keypress="formatDecimalValues(quantity)">
             </div>
             <input type="submit" id="submit-button" style="display: none;">
         </form>
@@ -28,10 +28,24 @@ export default {
         return {
             quantity: "",
             savingStock: false,
-            ingredients: []
+            ingredients: [],
+            selected_ingredient_measure_unit: "",
+            selected_ingredient: {}
         }
     },
     methods: {
+        selectThisIngredient: function () {
+            let value = $("#ingredient").val();
+            let ingredient = this.ingredients.find(obj => obj.id[1] == value);
+
+            if (ingredient != undefined && ingredient != null) {
+                this.selected_ingredient = ingredient;
+                this.selected_ingredient_measure_unit = ingredient.unidade_medida[1];
+            } else {
+                this.selected_ingredient = {};
+                this.selected_ingredient_measure_unit = "";
+            }
+        },
         saveStock: function () {
             let self = this;
 
