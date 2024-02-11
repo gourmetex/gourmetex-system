8a65689ff1f37c6bc49e7c0ed820b21366ede49d
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2024-02-10 23:11:58 -0300

    Troca url api

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 let url_api;
 
 const dev_environment = "http://192.168.15.167:3000/"; //Ambiente de desenvolvimento.
-const publish_environment = "https://gourmetech-coreteste-5d758e07e0da.herokuapp.com/"; //Ambiente de teste ou produção.
+const publish_environment = "https://gourmetech-core-46715094ffeb.herokuapp.com/"; //Ambiente de teste ou produção.
+
+// https://gourmetech-coreteste-5d758e07e0da.herokuapp.com/  --  ambiente de teste
+// https://gourmetech-core-46715094ffeb.herokuapp.com/       --  ambiente de produção         
+
 // TROCA DO AMBIENTE DA API
 //
 // O parâmetro que deverá ser informado será 0 ou 1, sendo que:
