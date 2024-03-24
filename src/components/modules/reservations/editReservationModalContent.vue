c3c9db82d5b639e5709b4b7684fd9df0ef7576f7
Saymon Felipe <linnubr@gmail.com>
2024-03-23 22:54:41 -0300

    Cademint tarefa #484

---
+++ src/components/modules/reservations/editReservationModalContent.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="edit-reservation-modal-content">
+        <form class="edit-customer" id="informations-form" @submit.prevent="saveReservation()">
+            <div class="form-group">
+                <label for="nome">Está no nome de</label>
+                <input type="text" name="nome" v-model="reservation.nome" placeholder="Ex. João" required>
+            </div>
+            <div class="form-group">
+                <label for="quantidade">Quantidade de pessoas</label>
+                <input type="number" name="quantidade" id="quantidade" v-model="reservation.quantidade" required>
+            </div>
+            <div class="form-group">
+                <label for="data">Data</label>
+                <input type="datetime-local" name="data" id="data" v-model="reservation.data" required>
+            </div>
+            <input type="submit" id="submit-button" style="display: none;">
+        </form>
+        <p class="response big">{{ response }}</p>
+    </div>
+</template>
+<script>
+import api from "../../../configs/api";
+import { globalMethods } from "@/js/globalMethods";
+import $ from 'jquery';
+
+export default {
+    name: "editReservationModalContent",
+    mixins: [globalMethods],
+    props: ["reservationid"],
+    data() {
+        return {
+            reservation: {
+                nome: "",
+                quantidade: null,
+                data: null
+            },
+            savingReservation: false
+        }
+    },
+    methods: {
+        returnReservation: function () {
+            let self = this;
+
+            if (self.customerid == null) {
+                return;
+            } 
+
+            api.get("/reservations/" + self.reservationid).then((response) => {
+                self.reservation = response.data.returnObj;
+            }).catch((error) => {
+                console.log(error);
+            })
+        },
+        saveReservation: function () {
+            let self = this;
+
+            if (self.savingReservation) return;
+
+            self.savingReservation = true;
+
+            let data = $("#informations-form").serializeArray().reduce(function (obj, item) { // Pega todos os dados do formulário e coloca em um objeto.
+                obj[item.name] = item.value;
+                return obj;
+            }, {});
+
+            let path = "new_reservation";
+
+            if (self.reservationid != null) {
+                path = "edit_reservation/" + self.reservationid;
+            }
+
+            api.post("/reservations/" + path, data).then(() => {
+                self.$emit("savedContent", true);
+            }).catch((error) => {
+                console.log(error);
+            }).then(() => {
+                self.savingCustomer = false;
+            })
+        }
+    },
+    mounted: function () {
+        this.returnReservation();        
+    }
+}
+</script>
+<style scoped>
+</style>
--- src/components/modules/reservations/reservationsComponent.vue
+++ src/components/modules/reservations/reservationsComponent.vue
@@ -3,12 +3,67 @@
         <div class="page-title">
             <h1>Reservas</h1>
         </div> 
-        <h2>Em breve</h2>
+        <actionButtons add_text="ADICIONAR RESERVA" exclude_text="EXCLUIR RESERVA" edit_text="EDITAR RESERVA" :disabledbuttons="disabledButtons" @add="createReservation()" @exclude="deleteReservation()" @edit="editReservation()" />
+        <div class="reservations-container">
+            <gridView :gridoptions="gridOptions" :griddata="reservationsList" @dataclick="selectRow($event)"></gridView>
+        </div>
+        <modal v-if="showModal" :modaltitle="modalTitle" :modalbutton1="modalButton1" :excludepath="'/orders/' + editId" :modalbutton2="modalButton2" :modalButton3="modalButton3" @closeModal="closeModalFunction(); returnReservations();">
+            <editReservationModalContent v-if="showEditReservationModalContent" :reservationid="editId" @savedContent="closeModalFunction(); returnReservations();"></editReservationModalContent>
+        </modal>
     </div>
 </template>
 <script>
+import actionButtons from "../../actionButtons.vue";
+import gridView from "../../gridView.vue";
+import { globalMethods } from "@/js/globalMethods";
+import editReservationModalContent from "./editReservationModalContent.vue";
+import modal from "../../modal.vue";
+import api from "../../../configs/api";
+
 export default {
-    name: "reservationsComponent"
+    name: "reservationsComponent",
+    mixins: [globalMethods],
+    data() {
+        return {
+            gridOptions: [],
+            reservationsList: [],
+            showEditReservationModalContent: false
+        }
+    },
+    methods: {
+        resetModalContents: function () {
+            this.showEditReservationModalContent = false;
+        }, 
+        createReservation: function () {
+            this.resetModalContents();
+            this.showModalFunction("Adicionar reserva", "Adicionar", "Cancelar");
+            this.showEditReservationModalContent = true;
+            this.editId = null;
+        },
+        deleteReservation: function () {
+            this.resetModalContents();
+            this.showModalFunction("Excluir reserva", "Excluir", "Cancelar");
+        },
+        editReservation: function () {
+            this.resetModalContents();
+            this.showModalFunction("Editar reserva", "Salvar", "Cancelar");
+            this.showEditReservationModalContent = true;
+        },
+        returnReservations: function () {
+            let self = this;
+
+            api.get("/reservations").then((response) => {
+                self.reservationsList = response.data.returnObj.reservationsList;
+                self.gridOptions = response.data.returnObj.labels;
+            })
+        }
+    },
+    components: {
+        actionButtons,
+        gridView,
+        modal,
+        editReservationModalContent
+    }
 }
 </script>
 <style scoped>
