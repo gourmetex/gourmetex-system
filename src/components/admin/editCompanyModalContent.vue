938b03308dfd0633ddcf7a36c5a2eb3235d2030d
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-02-11 01:23:02 -0300

    Tarefa #628

--- src/components/admin/editCompanyModalContent.vue
+++ src/components/admin/editCompanyModalContent.vue
@@ -1,17 +1,31 @@
 <template>
     <div class="edit-company-modal-content">
-        <form class="edit-company" id="informations-form" @submit.prevent="saveCompany()">
+        <form class="edit-company" id="informations-form" @submit.prevent="saveCompany()" v-if="company.id != null">
+            <h3 class="font-bold">Informações principais</h3>
             <div class="form-group">
                 <label for="name">Nome</label>
                 <input type="text" name="name" id="name" placeholder="Nome da empresa" v-model="company.nome" required>
+            </div>
+            <div class="form-group">
+                <label for="main-user">Usuário admin</label>
+                <input type="email" name="main_user_email" id="main-user" v-model="company.email_requisitado" placeholder="example@domain.com" required>
             </div>
             <div class="radio-group">
                 <input type="checkbox" name="active" id="active" v-model="company.ativa">
                 <label for="active">Ativo?</label>
             </div>
-            <div class="form-group">
-                <label for="main-user">Usuário admin</label>
-                <input type="email" name="main_user_email" id="main-user" v-model="company.email_requisitado" placeholder="example@domain.com" required>
+            <div class="company-modules">
+                <h3 class="font-bold">Módulos habilitados</h3>
+                <div class="radio-group" v-for="module in modules" :key="module.id">
+                    <input 
+                        type="checkbox" 
+                        :id="'module-' + module.id" 
+                        :value="module.id"
+                        v-model="activeModules"
+                        name="modules"
+                    >
+                    <label :for="'module-' + module.id">{{ module.nome }}</label>
+                </div>
             </div>
             <input type="submit" id="submit-button" style="display: none;">
         </form>
@@ -27,14 +41,24 @@ export default {
     data() {
         return {
             company: {
+                id: null,
                 nome: "",
                 ativa: false,
                 email_requisitado: ""
             },
-            savingCompany: false
+            savingCompany: false,
+            modules: [],
+            activeModules: []
         }
     },
     methods: {
+        returnModules: function () {
+            let self = this;
+
+            api.get("/companies/modules").then((response) => {
+                self.modules = response.data.returnObj;
+            })
+        },
         returnCompany: function () {
             let self = this;
             
@@ -42,6 +66,7 @@ export default {
 
             api.get("/companies/" + self.companyid + "/return_company").then((response) => {
                 self.company = response.data.returnObj;
+                self.activeModules = self.company.active_modules;
             }).catch((error) => {
                 console.log(error);
             })
@@ -60,6 +85,7 @@ export default {
 
             data["active"] = data.active == "on" ? true : false;
             data["company_id"] = self.companyid;
+            data["active_modules"] = self.activeModules;
 
             let path = "create";
 
@@ -77,6 +103,7 @@ export default {
         }
     },
     mounted: function () {
+        this.returnModules();
         if (this.companyid != 0) {
             $("#main-user").attr("disabled", "disabled");
         }
